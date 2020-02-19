5b33a8e2dc9c9f6cd1381be0433d552b6a0194b0
pousspouss <hugo.pousseur@gmail.com>
2020-02-19 16:58:04 +0100

    merge twist node inside example package

--- ros/pouco2000_ros_example/node/twist.cpp
+++ ros/pouco2000_ros_example/node/twist.cpp
@@ -9,15 +9,17 @@
 #include <boost/function.hpp>
 
 
+float convertPercentToLinearX(const float value){
+  return value/50;
+}
 
 void callback(const pouco2000_ros::Controller::ConstPtr& msg, ros::Publisher& pub){
     if(!msg->potentiometers_circle.data.empty()){
         geometry_msgs::Twist twist_msg;
         float value = msg->potentiometers_circle.data.at(0)/50;
-        twist_msg.linear.x = value;
+        twist_msg.linear.x = convertPercentToLinearX(value);
         pub.publish(twist_msg);
     }
-
 }
 
 int main(int argc, char **argv){
